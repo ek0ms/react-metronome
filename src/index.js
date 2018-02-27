36635355982120a38e6f6034b89c03715c13ec8b
Jeffrey Zhen <jzhen91@gmail.com>
2018-02-27 08:55:18 -0500

    handle bpm change

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import Metronome from "./Metronome";
+import registerServiceWorker from "./registerServiceWorker";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Metronome />, document.getElementById("root"));
 registerServiceWorker();
